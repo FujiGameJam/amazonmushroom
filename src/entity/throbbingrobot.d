e9a99933115be99209b0f77436c528f6185f4249
GooberMan <gooberman@gmail.com>
2013-08-31 03:35:28 +1000

    A whole heap of groundwork for the robot. Creation of entities is also rolled in to the codebase too. Woo.

--- src/entity/throbbingrobot.d
+++ src/entity/throbbingrobot.d
@@ -5,72 +5,100 @@ import interfaces.entity;
 import interfaces.renderable;
 import interfaces.collider;
 
+import camera.camera;
+
 class ThrobbingRobot : ISheeple, IEntity, IRenderable, ICollider
 {
+	struct ObjectState
+	{
+		MFMatrix		transform;
+		MFMatrix		prevTransform;
+
+		ISheeple.Moves	activeMoves = ISheeple.Moves.None;
+	}
+
+	private	Camera				camera = new Camera();
+	private ObjectState			currentState,
+								initialState;
+
+	private CollisionManager	collision = null;
+
+	@property Camera TrailingCamera()		{ return camera; }
+
 	///ISheeple
 	override void OnMove(MFVector direction)
 	{
+		currentState.prevTransform = currentState.transform;
+		currentState.transform.t += direction;
 	}
 
 	override @property bool CanMove()		{ return true; }
-	override @property bool IsRunning()		{ return false; }
+	override @property bool IsRunning()		{ return (currentState.activeMoves & ISheeple.Moves.Run) != 0; }
 
 	///IEntity
-	void OnCreate(ElementParser element)
+	override void OnCreate(ElementParser element)
 	{
 	}
 
-	void OnResolve(IEntity[string] loadedEntities)
+	override void OnResolve(IEntity[string] loadedEntities)
 	{
 	}
 
-	void OnReset()
+	override void OnReset()
 	{
+		currentState = initialState;
+		UpdateCamera();
 	}
 	
-	void OnDestroy()
+	override void OnDestroy()
 	{
 	}
 
 	// Do movement and other type logic in this one
-	void OnUpdate()
+	override void OnUpdate()
 	{
 	}
 
 	// Need to resolve post-movement collisions, such as punching someone? Here's the place to do it.
-	void OnPostUpdate()
+	override void OnPostUpdate()
 	{
 	}
 
-	@property bool CanUpdate()					{ return true; }
-	@property MFMatrix Transform()				{ return MFMatrix.identity; }
-	@property MFMatrix Transform(MFMatrix t)	{ return MFMatrix.identity; }
-	@property string Name()						{ return "Holy crap it's a throbbing robot!!!"; }
+	override @property bool CanUpdate()					{ return true; }
+	override @property MFMatrix Transform()				{ return currentState.transform; }
+	override @property MFMatrix Transform(MFMatrix t)	{ currentState.transform = t; return t; }
+	override @property string Name()					{ return "player1"; } // TODO: This is rather hacky - the entity creator expects the name to be player<X> with <X> being the correct number
 
 	///IRenderable
-	void OnRenderWorld()
+	override void OnRenderWorld()
 	{
 	}
 
-	void OnRenderGUI(MFRect orthoRect)
+	override void OnRenderGUI(MFRect orthoRect)
 	{
 	}
 
-	@property bool CanRenderWorld()		{ return true; }
-	@property bool CanRenderGUI()		{ return true; }
+	override @property bool CanRenderWorld()		{ return true; }
+	override @property bool CanRenderGUI()		{ return false; }
 
 	///ICollider
-	void OnAddCollision(CollisionManager owner)
+	override void OnAddCollision(CollisionManager owner)
 	{
+		collision = owner;
 	}
 
-	@property MFVector CollisionPosition()					{ return MFVector.zero; }
-	@property MFVector CollisionPosition(MFVector pos)		{ return MFVector.zero; }
+	override @property MFVector CollisionPosition()					{ return MFVector.zero; }
+	override @property MFVector CollisionPosition(MFVector pos)		{ return MFVector.zero; }
 
-	@property MFVector CollisionPrevPosition()				{ return MFVector.zero; }
+	override @property MFVector CollisionPrevPosition()				{ return MFVector.zero; }
 
-	@property CollisionType CollisionTypeEnum()				{ return CollisionType.Sphere; }
-	@property CollisionClass CollisionClassEnum()			{ return CollisionClass.Robot; }
-	@property MFVector CollisionParameters()				{ return MFVector.zero; }
+	override @property CollisionType CollisionTypeEnum()			{ return CollisionType.Sphere; }
+	override @property CollisionClass CollisionClassEnum()			{ return CollisionClass.Robot; }
+	override @property MFVector CollisionParameters()				{ return MFVector.zero; }
 
+	// Robot specific stuff
+	void UpdateCamera()
+	{
+		camera.Position = currentState.transform.t + MFVector(0.0, 0.5, -1.5);
+	}
 }
